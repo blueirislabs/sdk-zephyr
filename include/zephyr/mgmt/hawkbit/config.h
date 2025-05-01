6d4d5fa44c1247649414c3a3cd4cf9247c02abab
Neal Jackson <nealsjackson@gmail.com>
2025-05-01 10:03:26 -0600

    mgmt: hawkbit: change hawkbit interface to support ip addresses
    Previously, hawkbit interface only supported a url/hostname and a port,
    and internally it resolves to an IP address. This does not work for
    network layers that rely on NAT64, like OpenThread.  Zephyr's
    implementation of `getaddrinfo` is not aware of NAT64.  DNS will resolve
    an IPV4 address that needs to be converted to IPV6 with the NAT64
    prefix.
    
    Instead, this commit alters the Hawkbit interface to allow providing an
    IP address and hostname as a string, so an application can provide an
    already resolved IP address if desired. This commit also changes the
    usage of `hawkbit_runtime_config.server_addr` to point to an IP address,
    and uses a new variable `hawkbit_runtime_config.server_hostname` for the
    URL. If `server_addr` is not provided, hawkbit will resolve
    `server_hostname`.
    
    This commit also adds a check for hostname strings that are too long.
    Previously, hawkbit would simply truncate the hostname and quietly fail
    to connect. Now it returns an error to the application.
    
    This commit also changes how the controllerId is generated based on
    device id, and disentangles the two. The controllerId is what hawkbit
    uses to uniquely identify a device, and is not necessarily the same as
    the device id, and should be fully customizeable by the user if needed.
    Previously, all custom device ids were being prepended with
    `CONFIG_BOARD`. When a user selects `CONFIG_HAWKBIT_CUSTOM_DEVICE_ID`,
    they should be able to specify the full controllerId used with hawkbit.
    
    Signed-off-by: Neal Jackson neal@blueirislabs.com

--- include/zephyr/mgmt/hawkbit/config.h
+++ include/zephyr/mgmt/hawkbit/config.h
@@ -29,8 +29,10 @@
  * settings.
  */
 struct hawkbit_runtime_config {
-	/** Server address */
+	/** Server ip address */
 	char *server_addr;
+	/** Server hostname*/
+	char *server_hostname;
 	/** Server port */
 	uint16_t server_port;
 	/** Security token */
@@ -44,6 +46,7 @@ struct hawkbit_runtime_config {
  *
  * @param config Configuration settings to set.
  * @retval 0 on success.
+ * @retval -EINVAL if string length mismatch for server_hostname
  * @retval -EAGAIN if probe is currently running.
  */
 int hawkbit_set_config(struct hawkbit_runtime_config *config);
@@ -56,6 +59,27 @@ int hawkbit_set_config(struct hawkbit_runtime_config *config);
 struct hawkbit_runtime_config hawkbit_get_config(void);
 
 /**
+ * @brief Set the hawkBit server hostname.
+ *
+ * @param hostname_str Server hostname to set.
+ * @retval 0 on success.
+ * @retval -EINVAL if string length mismatch for server_hostname
+ * @retval -EAGAIN if probe is currently running.
+ */
+static inline int hawkbit_set_server_hostname(char *hostname_str)
+{
+	struct hawkbit_runtime_config set_config = {
+		.server_addr = NULL,
+		.server_hostname = hostname_str,
+		.server_port = 0,
+		.auth_token = NULL,
+		.tls_tag = 0,
+	};
+
+	return hawkbit_set_config(&set_config);
+}
+
+/**
  * @brief Set the hawkBit server address.
  *
  * @param addr_str Server address to set.
@@ -66,6 +90,7 @@ static inline int hawkbit_set_server_addr(char *addr_str)
 {
 	struct hawkbit_runtime_config set_config = {
 		.server_addr = addr_str,
+		.server_hostname = NULL,
 		.server_port = 0,
 		.auth_token = NULL,
 		.tls_tag = 0,
@@ -85,6 +110,7 @@ static inline int hawkbit_set_server_port(uint16_t port)
 {
 	struct hawkbit_runtime_config set_config = {
 		.server_addr = NULL,
+		.server_hostname = NULL,
 		.server_port = port,
 		.auth_token = NULL,
 		.tls_tag = 0,
@@ -104,6 +130,7 @@ static inline int hawkbit_set_ddi_security_token(char *token)
 {
 	struct hawkbit_runtime_config set_config = {
 		.server_addr = NULL,
+		.server_hostname = NULL,
 		.server_port = 0,
 		.auth_token = token,
 		.tls_tag = 0,
@@ -123,6 +150,7 @@ static inline int hawkbit_set_tls_tag(sec_tag_t tag)
 {
 	struct hawkbit_runtime_config set_config = {
 		.server_addr = NULL,
+		.server_hostname = NULL,
 		.server_port = 0,
 		.auth_token = NULL,
 		.tls_tag = tag,
@@ -139,6 +167,16 @@ static inline int hawkbit_set_tls_tag(sec_tag_t tag)
 static inline char *hawkbit_get_server_addr(void)
 {
 	return hawkbit_get_config().server_addr;
+}
+
+/**
+ * @brief Get the hawkBit server hostname.
+ *
+ * @return Server hostname.
+ */
+static inline char *hawkbit_get_server_hostname(void)
+{
+	return hawkbit_get_config().server_hostname;
 }
 
 /**
